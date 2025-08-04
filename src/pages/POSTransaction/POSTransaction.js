a56de1930fadc4588b0d02e50f72b238ecbcba0d
YaoMa2392 <ym2392@nyu.edu>
2025-08-04 01:13:06 -0400

    fix POSTransaction layout issue

--- src/pages/POSTransaction/POSTransaction.js
+++ src/pages/POSTransaction/POSTransaction.js
@@ -49,7 +49,7 @@ export default function POSTransaction() {
                     initial={{opacity: 0, y: -30}}
                     animate={{opacity: 1, y: 0}}
                     transition={{duration: 1}}
-                    className="live-title"
+                    className="pos-title"
                 >
                     POS Transaction
                 </motion.h1>
